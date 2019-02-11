fd33cd832f20bc9a57d723ffdb0f3d993907afee
castillocarlosr <castillocarlosr@gmail.com>
2019-02-10 16:47:48 -0800

    EC2 interview question 2

---
+++ Challenges/LeafsOfTree/LeafsBoolTwoTrees/LeafsBoolTwoTrees/Classes/Node.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeafsBoolTwoTrees.Classes
+{
+    public class Node
+    {
+        /// <summary>
+        /// Setting the value as a string which can be pathway for directory.
+        /// </summary>
+        public string FolderName { get; set; }
+
+        public Node LeftChild { get; set; }
+
+        public Node RightChild { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="folderName"></param>
+        public Node(string folderName)
+        {
+            FolderName = folderName;
+
+            LeftChild = RightChild = null;
+        }
+    }
+}
