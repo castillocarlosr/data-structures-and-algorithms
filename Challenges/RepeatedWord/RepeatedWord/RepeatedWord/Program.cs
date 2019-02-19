5af0c5837d74036f7df0a85ed8843f74c4e79dbe
castillocarlosr <castillocarlosr@gmail.com>
2019-02-18 18:23:42 -0800

    implemented code, still need tests

--- Challenges/RepeatedWord/RepeatedWord/RepeatedWord/Program.cs
+++ Challenges/RepeatedWord/RepeatedWord/RepeatedWord/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using HashTables.Classes;
+using HashTables;
 
 namespace RepeatedWord
 {
@@ -7,8 +9,37 @@ namespace RepeatedWord
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Repeated Word!");
+            Console.WriteLine(" ");
+
+            string input = "Once A upon a time two two ok";
+
+            Console.WriteLine($"First repeated word:  {firstRepeatedWord(input)}");
 
             Console.ReadLine();
         }
+        
+        public static string firstRepeatedWord(string input)
+        {
+            
+            string[] listOfWord = input.ToLower().Split(' ');
+            Hashtables hashWords = new Hashtables();
+
+            for (int i = 0; i < listOfWord.Length; i++)
+            {
+                string temp = listOfWord[i];
+
+                if (hashWords.Contains(temp))
+                {
+                    return temp;
+                }
+                else
+                {
+                    hashWords.Add(temp, temp);
+                }
+            }
+
+            return "No repeated word found.";
+             
+        } 
     }
 }
