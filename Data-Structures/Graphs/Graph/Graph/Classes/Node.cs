56f8c6de64dbcc5d5c2d4d5bc18a0dfeee43945f
castillocarlosr <castillocarlosr@gmail.com>
2019-02-23 16:25:01 -0800

    setup

---
+++ Data-Structures/Graphs/Graph/Graph/Classes/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Graph.Classes
+{
+    public class Node
+    {
+        public object Key { get; set; }
+
+        public object Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node Previous { get; set; }
+
+        public Node(object key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
---
+++ Data-Structures/Graphs/Graph/Graph/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Graphs");
+
+            Console.ReadLine();
+        }
+    }
+}
