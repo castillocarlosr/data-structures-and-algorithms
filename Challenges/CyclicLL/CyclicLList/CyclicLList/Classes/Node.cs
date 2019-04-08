2cb55f83705d597fba8c11d41b975c24ad73b222
castillocarlosr <castillocarlosr@gmail.com>
2019-04-07 21:36:52 -0700

    almost done.  Finish monday

---
+++ Challenges/CyclicLL/CyclicLList/CyclicLList/Classes/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyclicLList.Classes
+{
+    public class Node
+    {
+        /// <summary>
+        /// This is the value that goes in the node
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// This is the pointer to the next node
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Constructor for the nodes
+        /// </summary>
+        /// <param name="value">was int.  I changed it to object</param>
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+}
