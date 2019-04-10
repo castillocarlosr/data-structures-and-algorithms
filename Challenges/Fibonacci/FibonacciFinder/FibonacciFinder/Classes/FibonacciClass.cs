8600a08add2d85ec04c6f9b2398a935449506808
castillocarlosr <castillocarlosr@gmail.com>
2019-04-10 10:05:22 -0700

    recursive and iterative added

---
+++ Challenges/Fibonacci/FibonacciFinder/FibonacciFinder/Classes/FibonacciClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciFinder.Classes
+{
+    public class FibonacciClass
+    {
+        public int FindSpecificIterative(int input)
+        {
+            List<int> fibSequence = new List<int>() { 0, 1};
+            for (int i = 2; i < input + 1; i++)
+            {
+                fibSequence.Add(fibSequence[i-2] + fibSequence[i-1]);
+            }
+            return fibSequence[input];
+        }
+
+        public int FindSpecificRecursive(int input)
+        {
+            if(input < 2)
+            {
+                return input;
+            }
+            return FindSpecificRecursive(input - 1) + FindSpecificRecursive(input - 2);
+        }
+
+        //Fibonacci Sequence is 1,1,2,3,5,8,13,21,34,55,89,144
+        /*
+        public int[] PrintSequence(int input)
+        {
+            if(input < 2)
+            {
+                int[] singleFib = new int[1] { 1 };
+                return singleFib;
+            }
+            else
+            {
+                int[] fibArray = new int[input];
+                for (int i = 2; i < input; i++)
+                {
+                    fibArray
+                }
+            }
+
+            
+        }
+        */
+    }
+}
---
+++ Challenges/Fibonacci/FibonacciFinder/FibonacciFinder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FibonacciFinder.Classes;
+
+namespace FibonacciFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Golden Ratio");
+            Console.WriteLine("Fibonacci Sequence is 1,1,2,3,5,8,13,21,34,55,89,144, and that as far as I will go!");
+            Console.WriteLine("");
+
+            FibonacciClass fibonacciQuestion = new FibonacciClass();
+            Console.WriteLine("***************Iterative Approach*************");
+            Console.WriteLine($"12th number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificIterative(12)}");
+            Console.WriteLine($"1st number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificIterative(1)}");
+            Console.WriteLine($"2nd number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificIterative(2)}");
+            Console.WriteLine($"3rd number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificIterative(3)}");
+
+            Console.WriteLine("***************Recursive Approach*************");
+            Console.WriteLine($"12th number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificRecursive(12)}");
+            //Console.WriteLine($"1st number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificRecursive(1)}");
+            //Console.WriteLine($"2nd number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificRecursive(2)}");
+            Console.WriteLine($"3rd number in Fibonacci Sequence: {fibonacciQuestion.FindSpecificRecursive(3)}");
+
+            Console.ReadLine();
+        }
+    }
+}
