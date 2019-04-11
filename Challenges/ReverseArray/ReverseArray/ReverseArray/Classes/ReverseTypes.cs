e0739fa651bb01b8c67bf76beb8fe1fecba40781
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-04-11 12:28:00 -0700

    array reverse setup

---
+++ Challenges/ReverseArray/ReverseArray/ReverseArray/Classes/ReverseTypes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ReverseArray.Classes
+{
+    public class ReverseTypes
+    {
+        public ReverseTypes()
+        {
+        }
+    }
+}
---
+++ Challenges/ReverseArray/ReverseArray/ReverseArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Reverse Arrays and Reverse Strings!");
+
+            _ = Console.ReadLine();
+        }
+    }
+}
