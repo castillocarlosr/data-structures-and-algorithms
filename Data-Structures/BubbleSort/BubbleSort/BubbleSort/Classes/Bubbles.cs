14045100257e4f206b1f52556a4346a2a444a6dc
castillocarlosr <castillocarlosr@gmail.com>
2019-04-14 20:24:51 -0700

    done with code.  Needs readme

--- Data-Structures/BubbleSort/BubbleSort/BubbleSort/Classes/Bubbles.cs
+++ Data-Structures/BubbleSort/BubbleSort/BubbleSort/Classes/Bubbles.cs
@@ -23,22 +23,6 @@ namespace BubbleSort.Classes
             return input;
         }
 
-        public static void printEveryLine(int[] input)
-        {
-            for (int i = 0; i < input.Length - 1; i++)
-            {
-                for (int j = 0; j < input.Length - i - 1; j++)
-                {
-                    if (input[j] > input[j + 1])
-                    {
-                        int temp = input[j];
-                        input[j] = input[j + 1];
-                        input[j + 1] = temp;
-                    }
-                }
-                Console.Write($"This is pass {i}:");
-                Console.WriteLine($"{string.Join(" ", input)}");
-            }
-        }
+        
     }
 }
--- Data-Structures/BubbleSort/BubbleSort/BubbleSort/Program.cs
+++ Data-Structures/BubbleSort/BubbleSort/BubbleSort/Program.cs
@@ -12,11 +12,35 @@ namespace BubbleSort
             Console.WriteLine("");
 
             Bubbles bubbles = new Bubbles();
-            int[] myNumbers = new int[12] { 4, 76, 22, 23, 24, 67, 99, 14, 15, 10, 2, 8 };
+            int[] myNumbers = new int[12] { 4, 76, 22, 23, 24, 67, 99, 14, 15, 10, 8, 5 };
             //bubbles.numArrSort(myNumbers);
+            Console.WriteLine($"Input array: {string.Join(", ", myNumbers)}");
             Console.WriteLine($"Bubble Sorted:  {String.Join(", ", bubbles.numArrSort(myNumbers))}");
 
+            Console.WriteLine("");
+            Console.WriteLine("Printed line by line");
+            Console.WriteLine($"Input array: {string.Join(", ", myNumbers)}");
+            printsEveryLine(myNumbers);
+
             Console.ReadLine();
+        }
+
+        public static void printsEveryLine(int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = 0; j < input.Length - i - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        int temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = temp;
+                    }
+                }
+                Console.Write($"This is pass {i}:");
+                Console.WriteLine($"{string.Join(" ", input)}");
+            }
         }
     }
 }
