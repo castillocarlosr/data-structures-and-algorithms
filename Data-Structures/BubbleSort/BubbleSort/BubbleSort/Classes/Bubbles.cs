c11a006131835eb3bfeeaac9745ad4da5da5cc53
castillocarlosr <castillocarlosr@gmail.com>
2019-04-14 18:02:13 -0700

    bubble classes added

---
+++ Data-Structures/BubbleSort/BubbleSort/BubbleSort/Classes/Bubbles.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BubbleSort.Classes
+{
+    public class Bubbles
+    {
+        public int[] numArrSort(int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = 0; j < input.Length - i - 1; j++)
+                {
+                    if(input[j] > input[j + 1])
+                    {
+                        int temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = temp;
+                    } 
+                }
+            }
+            return input;
+        }
+
+        public static void printEveryLine(int[] input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                for (int j = 0; j < input.Length - i - 1; j++)
+                {
+                    if (input[j] > input[j + 1])
+                    {
+                        int temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = temp;
+                    }
+                }
+                Console.Write($"This is pass {i}:");
+                Console.WriteLine($"{string.Join(" ", input)}");
+            }
+        }
+    }
+}
---
+++ Data-Structures/BubbleSort/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using BubbleSort.Classes;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Bubble Sort World");
+            Console.WriteLine("Not the most efficient way to sort but the easiest to code before watching Game of Thrones.");
+            Console.WriteLine("");
+
+            Bubbles bubbles = new Bubbles();
+            int[] myNumbers = new int[12] { 4, 76, 22, 23, 24, 67, 99, 14, 15, 10, 2, 8 };
+            bubbles.numArrSort(myNumbers);
+            Console.WriteLine($"Bubble Sorted:  {bubbles.numArrSort(myNumbers)}");
+
+            Console.ReadLine();
+        }
+    }
+}
