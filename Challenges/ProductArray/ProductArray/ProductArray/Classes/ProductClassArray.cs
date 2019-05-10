7a00788f6fd02d17aad357c2c2ae57f0605a7231
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-05-10 16:53:47 -0700

    logic done

--- Challenges/ProductArray/ProductArray/ProductArray/Classes/ProductClassArray.cs
+++ Challenges/ProductArray/ProductArray/ProductArray/Classes/ProductClassArray.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+
 namespace ProductArray.Classes
 {
     public class ProductClassArray
     {
-        public int[] ProductClassArray(int[] input)
+        public int[] ProductArrayExceptIndex(int[] input)
         {
-            int[] leftArr = new int[input.Length - 1];
-            int[] rightArr = new int[input.Length - 1];
-            int[] productArr = new int[input.Length - 1];
+            int[] leftArr = new int[input.Length];
+            int[] rightArr = new int[input.Length];
+            int[] productArr = new int[input.Length];
 
+            leftArr[0] = 1;
+            rightArr[input.Length - 1] = 1;
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                leftArr[i + 1] = leftArr[i] * input[i];
+            }
+            for(int j = input.Length-1; j>0; j--)
+            {
+                rightArr[j - 1] = rightArr[j] * input[j];
+            }
+            for (int k = 0; k < input.Length; k++)
+            {
+                productArr[k] = rightArr[k] * leftArr[k];
+            }
+            return productArr;
         }
     }
 }
