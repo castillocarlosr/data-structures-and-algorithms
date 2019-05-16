369bcb6f8f00e1c81ee9f606f554023ab1d16786
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-05-15 20:34:21 -0700

    set up namespace

---
+++ Challenges/ClosestPerfectSquare/ClosestPerfectSquare/ClosestPerfectSquare/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClosestPerfectSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World of Perfect Square......");
+            Console.WriteLine(".......or at least closest to it.");
+
+            Console.ReadLine();
+        }
+    }
+}
