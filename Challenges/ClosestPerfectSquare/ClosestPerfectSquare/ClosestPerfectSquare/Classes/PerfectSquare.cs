6a1b2c337c489e5bf02583f1ebdc978231a6ed55
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-05-17 13:02:50 -0700

    finished code logic

--- Challenges/ClosestPerfectSquare/ClosestPerfectSquare/ClosestPerfectSquare/Classes/PerfectSquare.cs
+++ Challenges/ClosestPerfectSquare/ClosestPerfectSquare/ClosestPerfectSquare/Classes/PerfectSquare.cs
@@ -16,5 +16,59 @@ namespace ClosestPerfectSquare.Classes
                 return false;
             }
         }
+
+        public int FindClosestSquare(int input)
+        {
+            if (IsPerfectSquare(input) == true)
+            {
+                return input;
+            }
+            else
+            {
+                int aboveInput;
+                int belowInput;
+                int n1;
+                int n2;
+
+                n1 = input + 1;
+                while (true)
+                {
+                    if (IsPerfectSquare(n1) == true)
+                    {
+                        aboveInput = n1;
+                        break;
+                    }
+                    else
+                    {
+                        n1++;
+                    }
+                }
+
+                n2 = input - 1;
+                while (true)
+                {
+                    if (IsPerfectSquare(n2) == true)
+                    {
+                        belowInput = n2;
+                        break;
+                    }
+                    else
+                    {
+                        n2--;
+                    }
+                }
+
+                int diffAbove = aboveInput - input;
+                int diffBelow = input - belowInput;
+                if(diffAbove > diffBelow)
+                {
+                    return belowInput;
+                }
+                else
+                {
+                    return aboveInput;
+                }
+            }
+        }
     }
 }
