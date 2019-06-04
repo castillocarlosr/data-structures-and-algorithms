b9c872ca24b78107eee4922dc25dbb6a3473be86
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-06-03 18:48:51 -0700

    progress

--- Challenges/ClosestNodeBinaryTree/ClosestNodeBinaryTree/ClosestNodeBinaryTree/Classes/ClosestInTree.cs
+++ Challenges/ClosestNodeBinaryTree/ClosestNodeBinaryTree/ClosestNodeBinaryTree/Classes/ClosestInTree.cs
@@ -3,9 +3,24 @@ namespace ClosestNodeBinaryTree.Classes
 {
     public class ClosestInTree
     {
-        public static void MaximumDifference(Node node, int diff)
+        public Node ClosestNumber(Node nodeInput, int numberInput)
         {
-
+            int difference = Math.Abs(nodeInput.Value - numberInput);
+            if(nodeInput != null)
+            {
+                if(Math.Abs(nodeInput.Value-numberInput) < difference)
+                {
+                    difference = Math.Abs(nodeInput.Value - numberInput);
+                }
+                if(nodeInput.LeftChild != null)
+                {
+                    ClosestNumber(nodeInput.LeftChild, numberInput);
+                }
+                if(nodeInput.RightChild != null)
+                {
+                    ClosestNumber(nodeInput.RightChild, numberInput);
+                }
+            }
         }
     }
 }
