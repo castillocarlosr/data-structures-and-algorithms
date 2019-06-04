d49a32a88f5b11f4f87bb2beebe7c7bd296b5b60
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-06-03 19:07:31 -0700

    save work before testing

--- Challenges/ClosestNodeBinaryTree/ClosestNodeBinaryTree/ClosestNodeBinaryTree/Program.cs
+++ Challenges/ClosestNodeBinaryTree/ClosestNodeBinaryTree/ClosestNodeBinaryTree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClosestNodeBinaryTree.Classes;
 
 namespace ClosestNodeBinaryTree
 {
@@ -7,6 +8,29 @@ namespace ClosestNodeBinaryTree
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Binary Tree World!");
+
+            Node root = new Node(1);
+            Node two = new Node(2);
+            Node three = new Node(3);
+            Node four = new Node(4);
+            Node ten = new Node(10);
+            Node eleven = new Node(11);
+            Node fifteen = new Node(15);
+            Node twenty = new Node(20);
+            Node thirtyThree = new Node(33);
+
+            root.LeftChild = two;
+            root.RightChild = three;
+
+            two.LeftChild = thirtyThree;
+            two.RightChild = twenty;
+
+            three.LeftChild = four;
+            three.RightChild = ten;
+
+            thirtyThree.LeftChild = eleven;
+
+            ten.RightChild = fifteen;
 
             Console.ReadLine();
         }
