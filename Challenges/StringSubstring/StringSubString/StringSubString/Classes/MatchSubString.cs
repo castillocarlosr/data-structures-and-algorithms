5a8bc47dec73d0e0e1b4ff6fb87ed2385951a10b
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-08-05 16:55:04 -0700

    setup classes and code in class

---
+++ Challenges/StringSubstring/StringSubString/StringSubString/Classes/MatchSubString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace StringSubString.Classes
+{
+    public class MatchSubString
+    {
+        public bool MatchSubStringBool(string s1, string s2)
+        {
+            int s1Len = s1.Length;
+            int s2Len = s2.Length;
+
+            for (int i = 0; i < s2Len - s1Len; i++)
+            {
+                for (int j = 0; j < s1Len; j++)
+                {
+                    if(s2[i + j] != s1[j])
+                    {
+                        break;
+                    }
+                    if(j == s1Len)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Challenges/StringSubstring/StringSubString/StringSubString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringSubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
