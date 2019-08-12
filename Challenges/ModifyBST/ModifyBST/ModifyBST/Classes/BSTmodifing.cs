046105636cfbab8de0beef57576cbb579823292d
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-08-11 22:08:47 -0700

    completed.

--- Challenges/ModifyBST/ModifyBST/ModifyBST/Classes/BSTmodifing.cs
+++ Challenges/ModifyBST/ModifyBST/ModifyBST/Classes/BSTmodifing.cs
@@ -3,8 +3,24 @@ namespace ModifyBST.Classes
 {
     public class BSTmodifing
     {
-        public BSTmodifing()
+        public void addAllValuesGreater(Node node, int sum)
         {
+            if (node == null)
+            {
+                return;
+            }
+            addAllValuesGreater(node.RightChild, sum);
+
+            sum += node.Value;
+            node.Value = sum;
+
+            addAllValuesGreater(node.LeftChild, sum);
+        }
+
+        public void runModifyBST(Node root)
+        {
+            int sum = 0;
+            addAllValuesGreater(root, sum);
         }
     }
 }
--- Challenges/ModifyBST/ModifyBST/ModifyBST/Program.cs
+++ Challenges/ModifyBST/ModifyBST/ModifyBST/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ModifyBST.Classes;
 
 namespace ModifyBST
 {
@@ -6,7 +7,35 @@ namespace ModifyBST
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Modified Binary Search Tree!");
+
+            Node tree = new Node(5);
+            tree.RightChild = new Node(7);
+            tree.RightChild.LeftChild = new Node(6);
+            tree.RightChild.RightChild = new Node(8);
+            tree.LeftChild = new Node(3);
+            tree.LeftChild.RightChild = new Node(4);
+            tree.LeftChild.LeftChild = new Node(2);
+
+            Console.WriteLine("Initial Binary Search Tree example:");
+            Console.WriteLine($"          {tree.Value}");
+            Console.WriteLine($"         /  \\");
+            Console.WriteLine($"        {tree.LeftChild.Value}     {tree.RightChild.Value}");
+            Console.WriteLine($"       / \\   / \\");
+            Console.WriteLine($"      {tree.LeftChild.LeftChild.Value}   {tree.LeftChild.RightChild.Value}  {tree.RightChild.LeftChild.Value}   {tree.RightChild.RightChild.Value}");
+
+            Console.WriteLine("\n");
+            Console.WriteLine("After modify on binary search tree");
+
+            BSTmodifing bSTmodifing = new BSTmodifing();
+            bSTmodifing.runModifyBST(tree);
+            Console.WriteLine($"          {tree.Value}");
+            Console.WriteLine($"         /  \\");
+            Console.WriteLine($"        {tree.LeftChild.Value}     {tree.RightChild.Value}");
+            Console.WriteLine($"       / \\   / \\");
+            Console.WriteLine($"      {tree.LeftChild.LeftChild.Value}   {tree.LeftChild.RightChild.Value}  {tree.RightChild.LeftChild.Value}   {tree.RightChild.RightChild.Value}");
+
+            Console.ReadLine();
         }
     }
 }
