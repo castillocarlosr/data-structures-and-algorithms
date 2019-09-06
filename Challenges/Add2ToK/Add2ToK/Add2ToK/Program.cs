3810f34406d70e58b30d2f02e8afd6ce85db297a
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-09-05 17:10:03 -0700

    yup, it works

--- Challenges/Add2ToK/Add2ToK/Add2ToK/Program.cs
+++ Challenges/Add2ToK/Add2ToK/Add2ToK/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Add2ToK.Classes;
 
 namespace Add2ToK
 {
@@ -6,7 +7,20 @@ namespace Add2ToK
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello K value and Int array");
+            Console.WriteLine("Does any two values in an array equal to a given k value?");
+            Console.WriteLine("");
+
+            SumBoolK sumBoolK = new SumBoolK();
+
+            int[] inputOne = { 0, 1, 2, 3, 4 };
+            int[] inputTwo = { 111, 99, 88, 77, 66, 55, 44, 33, 22, 11 };
+            int k = 5;
+
+            Console.WriteLine($"{{0, 1, 2, 3, 4}} and k=5 : {sumBoolK.TwoNumInK(inputOne, k)}");
+            Console.WriteLine($"{{111, 99, 88, 77, 66, 55, 44, 33, 22, 11}} and k=5 : {sumBoolK.TwoNumInK(inputTwo, k)}");
+
+            Console.ReadLine();
         }
     }
 }
