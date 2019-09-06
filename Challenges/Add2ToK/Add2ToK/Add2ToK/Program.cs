4e84ddfdefb710d4bf6f946c801e48d5d074a12e
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-09-05 17:02:37 -0700

    set up quick DSA question

---
+++ Challenges/Add2ToK/Add2ToK/Add2ToK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Add2ToK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
