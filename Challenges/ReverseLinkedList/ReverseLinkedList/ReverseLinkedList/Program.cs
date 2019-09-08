6af27246b45c7cd3398f45060f8fd2cdef808ff3
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-09-08 13:20:37 -0700

    set-up

---
+++ Challenges/ReverseLinkedList/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReverseLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
