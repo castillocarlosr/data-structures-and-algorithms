2b9b61beda32a70c0b27cdb66a07d5c61e036896
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-09-08 13:35:25 -0700

    added and finished classses

---
+++ Challenges/ReverseLinkedList/ReverseLinkedList/ReverseLinkedList/Classes/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ReverseLinkedList.Classes
+{
+    public class Node
+    {
+        /// <summary>
+        /// This is the value that goes in the node
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// This is the pointer to the next node
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Constructor for the nodes
+        /// </summary>
+        /// <param name="value">was int.  I changed it to object</param>
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
---
+++ Challenges/ReverseLinkedList/ReverseLinkedList/ReverseLinkedList/Classes/ReverseLL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace ReverseLinkedList.Classes
+{
+    public class ReverseLL
+    {
+        /// <summary>
+        /// This will always point to the front first node.  Never moves unless new node is inserted.
+        /// </summary>
+        public Node Head { get; set; }
+
+        /// <summary>
+        /// This will point to the current node through traversing the linked list.
+        /// </summary>
+        public Node Current { get; set; }
+
+        /// <summary>
+        /// This will print the node results out to the display when called up.
+        /// </summary>
+        /// <return>void</return>
+        public void Print()
+        {
+            try
+            {
+                if (Head != null)
+                {
+                    Current = Head;
+                    while (Current.Next != null)
+                    {
+                        Console.Write($"{Current.Value}->");
+                        Current = Current.Next;
+                    }
+                    Console.WriteLine($"{Current.Value}->null");
+                    Current = Head;
+                }
+                else
+                {
+                    Console.WriteLine("Your Linked-List is empty!!");
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Oh no error!: {error.Message}");
+            }
+        }
+
+        /// <summary>
+        /// This is where the magic happens
+        /// </summary>
+        public void ActionToReverseLL()
+        {
+            Node prev = null;
+            Node nextTemp = null;
+            Current = Head;
+            while(Current != null)
+            {
+                nextTemp = Current.Next;
+                Current.Next = prev;
+                prev = Current;
+                Current = nextTemp;
+            }
+            Head = prev;
+        }
+    }
+}
