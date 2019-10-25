1e0accf42d58cf0708286a17e86336906b246449
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-10-25 12:51:53 -0700

    oh no.  index out of range

--- Challenges/PreserveSpaceReverseString/PreserveSpaceReverseString/PreserveSpaceReverseString/Program.cs
+++ Challenges/PreserveSpaceReverseString/PreserveSpaceReverseString/PreserveSpaceReverseString/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using PreserveSpaceReverseString.Classes;
 
 namespace PreserveSpaceReverseString
 {
@@ -6,7 +7,24 @@ namespace PreserveSpaceReverseString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello.  Long time data structures.");
+            Console.WriteLine("Hello new challenge.");
+            Console.WriteLine("This program will reverse any string input except for the \"$\" sign.");
+            Console.WriteLine("That will stay in the same place no matter what.");
+            Console.WriteLine("");
+
+            PreservingSpaceClass stringOne = new PreservingSpaceClass();
+
+            string inputOne = "12345.  This sentence has no dollar signs. 6789";
+            stringOne.ReverseInputStr(inputOne);
+            string inputTwo = "12345.  This sentence has one $ dollar signs. 6789";
+            stringOne.ReverseInputStr(inputTwo);
+            string inputThree = "12345.  $This $ sentence has multiple $$ dol$lar signs$. 6789";
+            stringOne.ReverseInputStr(inputThree);
+
+            Console.Write("Enter your own text:  ");
+            string inputFour = Console.ReadLine();
+
+            stringOne.ReverseInputStr(inputFour);
             Console.ReadLine();
         }
     }
