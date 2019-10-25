b366d3fbb07040f97284fcb9770e6d713ae19d27
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-10-25 11:55:26 -0700

    set up new challenge.  Preserve spaces in reverse function

---
+++ Challenges/PreserveSpaceReverseString/PreserveSpaceReverseString/PreserveSpaceReverseString/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PreserveSpaceReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.  Long time data structures.");
+            Console.ReadLine();
+        }
+    }
+}
