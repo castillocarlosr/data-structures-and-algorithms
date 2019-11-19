57a29ebf4e4e42fa6987dc99682bd5d533db3e07
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2019-11-18 16:02:43 -0800

    removed commented out code

--- Data-Structures/InsertionSort/InsertionSort/InsertionSort/Program.cs
+++ Data-Structures/InsertionSort/InsertionSort/InsertionSort/Program.cs
@@ -38,26 +38,5 @@ namespace InsertionSort
             }
             return array;
         }
-
-        //Below is to display on the console the method
-        /*
-        public static void InsertionSort(int[] array)
-        {
-            for(int i=1; i < array.Length; i++)
-            {
-                int j = i - 1;
-                int temp = array[i];
-
-                while(j >= 0 && temp < array[j])
-                {
-                    array[j + 1] = array[j];
-                    j--;
-                }
-                array[j + 1] = temp;
-                Console.Write("After pass " + i + " : ");
-                Console.WriteLine(String.Join(" ", array));
-            }
-        }
-        */
     }
 }
