2c788e2316be12cc3764a8a7b8addbb34c42d906
castillocarlosr <castillocarlosr@gmail.com>
2020-01-15 17:04:16 -0800

    created solution and class template

---
+++ Data-Structures/MergeSort/MergeSort/MergeSort/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Merge Sort World!");
+
+            Console.ReadLine();
+        }
+    }
+}
