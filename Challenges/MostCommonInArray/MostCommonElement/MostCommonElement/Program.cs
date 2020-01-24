7ecb39bb1e306357d51634fb732e21c74e66c27f
castillocarlosr <castillocarlosr@gmail.com>
2020-01-24 12:24:27 -0800

    created solution

---
+++ Challenges/MostCommonInArray/MostCommonElement/MostCommonElement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MostCommonElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.  I looking for the most common elelement in an arrayyyyyy!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
