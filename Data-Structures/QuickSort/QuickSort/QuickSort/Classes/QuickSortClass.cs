509d62d096e7e001e1f376cdf305abcc7dce9323
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2020-01-26 12:43:58 -0800

    added classes.  not yet logic added

---
+++ Data-Structures/QuickSort/QuickSort/QuickSort/Classes/QuickSortClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace QuickSort.Classes
+{
+    public class QuickSortClass
+    {
+        public int[] QuickSortObject()
+        {
+            int[] result = new int[4];
+            //similar ro merge sort
+            //divide and conque
+            //will rerequire one or 3 pivots....I'll have to draw it out
+            return result;
+        }
+    }
+}
