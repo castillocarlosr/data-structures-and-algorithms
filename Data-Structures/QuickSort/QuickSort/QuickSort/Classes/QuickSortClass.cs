e408216b1df351a2c846157786d596c9f10fa8f0
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2020-01-27 15:01:12 -0800

    building class quicksort objecrts

--- Data-Structures/QuickSort/QuickSort/QuickSort/Classes/QuickSortClass.cs
+++ Data-Structures/QuickSort/QuickSort/QuickSort/Classes/QuickSortClass.cs
@@ -3,13 +3,31 @@ namespace QuickSort.Classes
 {
     public class QuickSortClass
     {
-        public int[] QuickSortObject()
+        /// <summary>
+        /// This is where the initial array will come in.
+        /// Getting the length and then calling the object for the low and high pivots.
+        /// </summary>
+        /// <param name="arr"></param>
+        public void QuickSortMain(int[] arr)
         {
-            int[] result = new int[4];
+            int high = arr.Length;
+            int low = 0;
+
+            QuickSortObject(arr, low, high);
+        }
+
+        static void QuickSortObject(int[] arr, int low, int high)
+        {
             //similar ro merge sort
             //divide and conque
             //will rerequire one or 3 pivots....I'll have to draw it out
-            return result;
+            if(low < high)
+            {
+                //int something = callAnotherObject(arr, lowPivot, highPivot);
+                // sort arr, low, pivot-1
+                //sort arr, pivot+1, high
+            }
+
         }
     }
 }
