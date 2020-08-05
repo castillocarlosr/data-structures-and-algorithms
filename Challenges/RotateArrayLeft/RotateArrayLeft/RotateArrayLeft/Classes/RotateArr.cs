8c75266d6cc296ebd602891908b25e68987367b8
castillocarlosr <castillocarlosr@gmail.com>
2020-08-05 09:42:57 -0700

    left array rotate complete

---
+++ Challenges/RotateArrayLeft/RotateArrayLeft/RotateArrayLeft/Classes/RotateArr.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RotateArrayLeft.Classes
+{
+    public class RotateArr
+    {
+        public int[] LeftRotate(int[] arr, int d)
+        {
+            /*
+            int[] result = new int[arr.Length];
+            for (int i = d, j=0; i < arr.Length; i++, j++)
+            {  
+            result[j] = arr[i];
+            }
+            for (int k = 0, l=d-1; k < d; k++, l++)
+            {
+            result[l] = arr[k];
+            }
+            return result;
+            */
+            d = d % arr.Length;
+            int[] buffer = new int[d];
+            Array.Copy(arr, buffer, d);
+            Array.Copy(arr, d, arr, 0, arr.Length - d);
+            Array.Copy(buffer, 0, arr, arr.Length - d, d);
+            return arr;
+        }
+
+        /****  Right Rotate Array coming sooon  ****/
+    }
+}
--- Challenges/RotateArrayLeft/RotateArrayLeft/RotateArrayLeft/Program.cs
+++ Challenges/RotateArrayLeft/RotateArrayLeft/RotateArrayLeft/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using RotateArrayLeft.Classes;
 
 namespace RotateArrayLeft
 {
@@ -6,7 +7,18 @@ namespace RotateArrayLeft
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("hello meee");
+            int[] test1 = new int[] { 1, 2, 3, 4, 5 };
+            int d1 = 3;
+
+            Console.WriteLine("Input1: [{0}]", string.Join(", ", test1));
+            RotateArr rotateArr1 = new RotateArr();
+            int[] answer1 = rotateArr1.LeftRotate(test1, d1);
+
+
+            Console.WriteLine("Output: [{0}]", string.Join(", ", answer1));
+
+            Console.ReadLine();
         }
     }
 }
