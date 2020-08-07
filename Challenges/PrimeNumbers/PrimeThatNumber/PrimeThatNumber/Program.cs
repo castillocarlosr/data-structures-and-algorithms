c5cdeb3aad2afd968b03b6d2e859de220c8a4fab
castillocarlosr <castillocarlosr@gmail.com>
2020-08-07 11:13:25 -0700

    created solution

---
+++ Challenges/PrimeNumbers/PrimeThatNumber/PrimeThatNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeThatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello PRIME World!");
+            Console.ReadLine();
+        }
+    }
+}
