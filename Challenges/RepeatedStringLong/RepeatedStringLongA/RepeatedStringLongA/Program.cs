1f0f360b680d444b0685281ccb1e07ba560208a4
castillocarlosr <castillocarlosr@gmail.com>
2020-09-01 16:41:11 -0700

    created solution

---
+++ Challenges/RepeatedStringLong/RepeatedStringLongA/RepeatedStringLongA/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RepeatedStringLongA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Sept 1 2020!");
+
+            Console.ReadLine();
+        }
+    }
+}
