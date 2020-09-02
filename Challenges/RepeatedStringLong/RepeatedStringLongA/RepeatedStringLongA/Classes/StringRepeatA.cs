027116366449720268632fdca78d86b8593a8f97
castillocarlosr <castillocarlosr@gmail.com>
2020-09-01 18:03:05 -0700

    finished challenge

---
+++ Challenges/RepeatedStringLong/RepeatedStringLongA/RepeatedStringLongA/Classes/StringRepeatA.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepeatedStringLongA.Classes
+{
+    public class StringRepeatA
+    {
+        public long repeatedString(string s, long n)
+        {
+            long strLength = s.Length;
+            char[] strChar = s.ToCharArray();
+            if (strLength == 1 && strChar[0] == 'a')
+            {
+                return n;
+            }
+
+            long counter = 0;
+            long remainderS = n % strLength;
+            long repeatedS = n / strLength;
+            for (long i = 0; i < strLength; i++)
+            {
+                if (strChar[i] == 'a')
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 0)
+            {
+                return 0;
+            }
+            else
+            {
+
+                long multiplyRepeatedS = repeatedS * counter;
+                if (remainderS == 0)
+                {
+                    return multiplyRepeatedS;
+                }
+                else
+                {
+                    for (long j = 0; j < remainderS; j++)
+                    {
+                        if (strChar[j] == 'a')
+                        {
+                            multiplyRepeatedS++;
+                        }
+                    }
+                    return multiplyRepeatedS;
+                }
+
+            }
+
+        }
+    }
+}
