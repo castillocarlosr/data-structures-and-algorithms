a8ea529d09326ecfc25cec75485645264b4b6967
CARLOS R. CASTILLO <castillocarlosr@gmail.com>
2020-09-05 15:37:54 -0700

    created starting solution

---
+++ Challenges/BribeQueue/BribeQueueLine/BribeQueueLine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BribeQueueLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!  Here's a few Benjamins for to look the other way.");
+
+            Console.ReadLine();
+        }
+    }
+}
