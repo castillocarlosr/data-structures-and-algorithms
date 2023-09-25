e3d8f59e0cfd00e153c8ccddb60f36096e1f8185
Carlos <castillocarlosr@gmail.com>
2023-09-25 15:39:43 -0700

    setup saved

---
+++ Challenges/BST_RangeInclusive/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace BST_RangeInclusive
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("DOG");
+        }
+        public class Node
+        {
+            int data;
+            Node? left;
+            Node? right;
+            public Node(int data)
+            {
+                this.data = data;
+                left = null;
+                right = null;
+            }
+        }
+
+        public class BST
+        {
+            public int RangeSum(Node head, int lower, int upper)
+            {
+                int answer = 0;
+                Console.WriteLine("Hello, Carlos!");
+                return answer;
+            }
+        }
+    }
+}
+
